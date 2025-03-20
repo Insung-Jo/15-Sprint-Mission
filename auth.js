4316c437dfcd6cf0334d3d26117083b134b6a098
조인성 <leeming01@naver.com>
2025-03-21 02:56:00 +0900

    WIP: 작업 중

--- auth.js
+++ auth.js
@@ -1,16 +1,25 @@
+const input = document.querySelectorAll('.auth__input');
+const button = document.querySelector('.deactivate-button');
+
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
+const passwordConfirm = document.querySelector('#password-confirm');
+const nickName = document.querySelector('#nickname');
+
 const emailError = document.querySelector('.email-error');
 const paswordError = document.querySelector('.pasword-error');
-const submitButton = document.querySelector('.auth__submit-button');
-const input = document.querySelectorAll('.auth__input');
+const nickNameError = document.querySelector('.nickname-error');
+const confirmError = document.querySelector('.confirm-error');
+
+const patternEmail = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+const patternPassword = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
+
 
 email.addEventListener('focusout', () => {
-    const pattern = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
-    if (email.value == '') {
+    if (email.value === '') {
         emailError.textContent = '이메일을 입력해주세요.';
         email.classList.add('error-input');
-    } else if (pattern.test(email.value) === true) {
+    } else if (patternEmail.test(email.value) === true) {
         emailError.textContent = '';
         email.classList.remove('error-input');
     } else {
@@ -20,11 +29,10 @@ email.addEventListener('focusout', () => {
 });
 
 password.addEventListener('focusout', () => {
-    const pattern = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
-    if (password.value == '') {
+    if (password.value === '') {
         paswordError.textContent = '비밀번호를 입력해주세요.';
         password.classList.add('error-input');
-    } else if (pattern.test(password.value) === true) {
+    } else if (patternPassword.test(password.value) === true) {
         paswordError.textContent = '';
         password.classList.remove('error-input');
     } else {
@@ -33,8 +41,57 @@ password.addEventListener('focusout', () => {
     }
 });
 
-for (let i = 0; i < input.length; i++){
+// nickName.addEventListener('focusout', () => {
+//     if (nickName.value === '') {
+//         nickNameError.textContent = '닉네임을 입력해주세요.';
+//         nickName.classList.add('error-input');
+//     } else {
+//         paswordError.textContent = '';
+//         password.classList.remove('error-input');
+//     }
+// });
+
+// passwordConfirm.addEventListener('focusout', () => {
+//     if (password.value !== passwordConfirm.value) {
+//         confirmError.textContent = '비밀번호가 일치하지 않습니다.';
+//         passwordConfirm.classList.add('error-input');
+//     } else {
+//         confirmError.textContent = '';r
+//         passwordConfirm.classList.remove('error-input');
+//     }
+// })
+
+for (let i = 0; i < input.length; i++) {
     input[i].addEventListener('focusout', () => {
-        if (paswordError.value === '' && emailError.test === t || input[i].value !== '') console.log('없음음');
+        if (patternEmail.test(email.value) === true && patternPassword.test(password.value) === true && input.includes('')) {
+            button.disabled = false;
+            button.classList.add('button');
+        } else {
+            button.disabled = true;
+            button.classList.remove('button');
+        }
     });
 }
+
+
+
+// function checkInput() {
+//     if (input.length === 2) {
+//         if (patternEmail.test(email.value) === true && patternPassword.test(password.value) === true && input[0].value !== '' && input[1].value !== '') {
+//             button.disabled = false;
+//             button.classList.add('button');
+//         } else {
+//             button.disabled = true;
+//             button.classList.remove('button');
+//         }
+//     } else {
+//         if (patternEmail.test(email.value) === true && patternPassword.test(password.value) === true && ) {
+//             button.disabled = false;
+//             button.classList.add('button');
+//         } else {
+//             button.disabled = true;
+//             button.classList.remove('button');
+//         }
+//     }
+    
+// }
