1edbc6a365eda4191d26d0c6ddfb7bed1090a5f6
조인성 <leeming01@naver.com>
2025-04-13 03:39:12 +0900

    feat: 전역 스타일 추가 및 반영

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+import { Global } from "@emotion/react";
 import Header from "./components/Header/Header";
 import MainPage from "./pages/MainPage";
 import ItemPage from "./pages/ItemPage";
+import GlobalStyles from "./styles/GlobalStyles";
 
+/** @jsxImportSource @emotion/react */
 function App() {
   return (
     <>
+      <Global styles={GlobalStyles} />
       <Routes>
         <Route element={<Header />}>
           <Route index element={<MainPage />} />
---
+++ src/styles/GlobalStyles.js
@@ -0,0 +1,29 @@
+import { css } from "@emotion/react";
+
+const GlobalStyles = css`
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    font-family: "Pretendard", sans-serif;
+    font-size: 62.5%;
+    vertical-align: baseline;
+  }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
+
+  ol,
+  ul {
+    list-style: none;
+  }
+`;
+
+export default GlobalStyles;
