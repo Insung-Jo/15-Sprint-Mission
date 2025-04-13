1297b6bc2f017eed27567b8adbb3dfce47903a81
조인성 <leeming01@naver.com>
2025-04-14 02:13:04 +0900

    refactor: 커스텀 훅 적용 및 리팩토링

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useState } from "react";
 import { getData } from "../api/api";
 import Item from "../components/item/Item";
-import { pageInner } from "./pages.style";
+import { ItemPageLayout } from "./pages.style";
 import { titleStyle } from "../styles/common";
+import usePageSize from "../hooks/usePageSize";
 
 /** @jsxImportSource @emotion/react */
 const ItemPage = () => {
   const [item, setItem] = useState([]);
   const [best, setBest] = useState([]);
+  const pageSize = usePageSize(10, 4, 6);
+  const bestPageSize = usePageSize(4, 1, 2);
 
   const fetchData = async (option) => {
     const { list } = await getData(option);
@@ -19,15 +22,15 @@ const ItemPage = () => {
     setBest(list);
   };
   useEffect(() => {
-    fetchData({ page: 1, pageSize: 10, orderBy: "favorite", keyword: "" });
-  }, []);
+    fetchData({ page: 1, pageSize: pageSize, orderBy: "recent", keyword: "" });
+  }, [pageSize]);
 
   useEffect(() => {
-    fetcBesthData({ pageSize: 4, orderBy: "favorite" });
-  }, []);
+    fetcBesthData({ pageSize: bestPageSize, orderBy: "favorite" });
+  }, [bestPageSize]);
 
   return (
-    <div css={pageInner}>
+    <div css={ItemPageLayout}>
       <div css={titleStyle}>베스트 상품</div>
       <Item items={best} variant={true} />
       <Item items={item} variant={false} />
