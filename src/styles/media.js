67a1aa183b9b9ff6ece441fae00de641addf1700
조인성 <leeming01@naver.com>
2025-04-14 01:20:44 +0900

    style: 브레이크포인트 수정

--- src/styles/media.js
+++ src/styles/media.js
@@ -1,6 +1,6 @@
 import facepaint from "facepaint";
 
-const breackpoints = [410, 1200, 1920];
+const breackpoints = [740, 1200, 1920];
 
 const mq = facepaint(breackpoints.map((bp) => `@media (min-width: ${bp}px)`));
 
