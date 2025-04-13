990457fcb47a7108f7e5c8deb8034678bf7442f8
조인성 <leeming01@naver.com>
2025-04-13 16:02:31 +0900

    feat : Profile 스타일 추가 및 반영

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,9 @@
 import DefaultProfile from "../../assets/default/default_profile.png";
+import ProfileStyle from "./Profile.styles";
 
+/** @jsxImportSource @emotion/react */
 const Profile = () => {
-  return <img src={DefaultProfile} />;
+  return <img css={ProfileStyle} src={DefaultProfile} />;
 };
 
 export default Profile;
