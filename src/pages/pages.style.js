40e9bb8e7839cd511a5ed43cc4c797635a730263
조인성 <leeming01@naver.com>
2025-04-14 02:05:20 +0900

    refactor: Pages 스타일 수정

--- src/pages/pages.style.js
+++ src/pages/pages.style.js
@@ -1,8 +1,14 @@
 import { css } from "@emotion/react";
+import mq from "../styles/media";
 
-const pageInner = css`
-  width: 120rem;
-  margin: 3.2rem auto;
+const ItemPageLayout = css`
+  width: 34.3rem;
+  margin: 1.6rem auto;
+
+  ${mq({
+    width: [undefined, "69.8rem", "120rem"],
+    margin: [undefined, "2.4rem auto", "3.2rem auto"],
+  })}
 `;
 
-export { pageInner };
+export { ItemPageLayout };
