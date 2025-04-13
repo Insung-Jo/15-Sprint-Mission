40c692fe53b2e10ab2362fdde73c5494aaeb3378
조인성 <leeming01@naver.com>
2025-04-13 18:44:25 +0900

    feat: API 연동 작업 및 페이지 스타일 추가

--- src/pages/ItemPage.jsx
+++ src/pages/ItemPage.jsx
@@ -1,5 +1,38 @@
+import { useEffect, useState } from "react";
+import { getData } from "../api/api";
+import Item from "../components/item/Item";
+import { pageInner } from "./pages.style";
+import { titleStyle } from "../styles/common";
+
+/** @jsxImportSource @emotion/react */
 const ItemPage = () => {
-  return <div>아이템 페이지 입니다.</div>;
+  const [item, setItem] = useState([]);
+  const [best, setBest] = useState([]);
+
+  const fetchData = async (option) => {
+    const { list } = await getData(option);
+    setItem(list);
+  };
+
+  const fetcBesthData = async (option) => {
+    const { list } = await getData(option);
+    setBest(list);
+  };
+  useEffect(() => {
+    fetchData({ page: 1, pageSize: 10, orderBy: "favorite", keyword: "" });
+  }, []);
+
+  useEffect(() => {
+    fetcBesthData({ pageSize: 4, orderBy: "favorite" });
+  }, []);
+
+  return (
+    <div css={pageInner}>
+      <div css={titleStyle}>베스트 상품</div>
+      <Item items={best} variant={true} />
+      <Item items={item} variant={false} />
+    </div>
+  );
 };
 
 export default ItemPage;
---
+++ src/pages/pages.style.js
@@ -0,0 +1,8 @@
+import { css } from "@emotion/react";
+
+const pageInner = css`
+  width: 120rem;
+  margin: 3.2rem auto;
+`;
+
+export { pageInner };
