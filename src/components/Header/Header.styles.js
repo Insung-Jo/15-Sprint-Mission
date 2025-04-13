7274e63d8233cb9bbcf9ddd2664cefdf5a433bb2
조인성 <leeming01@naver.com>
2025-04-13 16:26:48 +0900

    refactor: 스타일 구조 변경

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -1,17 +1,18 @@
 import { css } from "@emotion/react";
 import mq from "../../styles/media";
 
-const HeaderStyle = css(
-  mq({
-    paddingInline: ["2.4rem", "2.4rem", "20rem"],
-    paddingBlock: "0.9rem",
-    borderBottom: "0.1rem solid #dfdfdf",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-  })
-);
+const HeaderStyle = css`
+  padding-inline: 20rem;
+  padding-block: 0.9rem;
+  border-bottom: 0.1rem solid #dfdfdf;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
+  ${mq({
+    paddingInline: ["2.4rem", "2.4rem", "20rem"],
+  })}
+`;
 const containerStyle = css`
   display: flex;
   align-items: center;
--- src/components/Logo/Logo.styles.js
+++ src/components/Logo/Logo.styles.js
@@ -1,8 +1,14 @@
 import { css } from "@emotion/react";
 import mq from "../../styles/media";
 
-const LogoStyle = css(
-  mq({ width: ["8.1rem", "15.3rem"], height: ["2.7rem", "5.1rem"] })
-);
+const LogoStyle = css`
+  width: 15.3rem;
+  height: 5.1rem;
+
+  ${mq({
+    width: ["8.1rem", "15.3rem"],
+    height: ["2.7rem", "5.1rem"],
+  })}
+`;
 
 export default LogoStyle;
